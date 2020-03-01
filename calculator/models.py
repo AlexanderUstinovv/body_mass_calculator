651085c45f94997d3ea88aa8315514652e7a6092
Alexander Ustinov <alexander.a.ustinov@gmail.com>
2020-03-01 16:37:09 +0300

    Added models, body mass index calculation logic

--- calculator/models.py
+++ calculator/models.py
@@ -1,3 +1,47 @@
-from django.db import models
+from math import pow
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class MainPersonData(models.Model):
+    class Meta:
+        db_table = 'main_data'
+        constraints = (
+            models.CheckConstraint(check=models.Q(age__gte=18), name='age_gte_18'),
+        )
+
+    MALE = 'M'
+    FEMALE = 'F'
+    SEX_TYPES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+    )
+
+    person = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=15)
+    sex = models.CharField(max_length=6, choices=SEX_TYPES)
+    age = models.PositiveSmallIntegerField()
+    height = models.PositiveSmallIntegerField()
+    weight = models.PositiveSmallIntegerField()
+
+
+class BodyMassIndex(models.Model):
+    class Meta:
+        db_table = 'body_mass_index'
+        constraints = (
+            models.CheckConstraint(check=models.Q(value__gte=0), name='value_gte_0'),
+        )
+
+    value = models.DecimalField(max_digits=5, decimal_places=2)
+    person = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+@receiver(post_save, sender=MainPersonData, dispatch_uid='calculate_body_mass_index_value')
+def calculate_body_mass_index(sender, instance, **kwargs) -> None:
+    user = instance.person
+    value = instance.weight / pow((instance.height / 100), 2)
+    body_mass_index = BodyMassIndex(value=value, person=user)
+    body_mass_index.save()
--- calculator/tests.py
+++ calculator/tests.py
@@ -1,3 +1,54 @@
-from django.test import TestCase
+from decimal import Decimal
 
-# Create your tests here.
+from django.test import TestCase, TransactionTestCase
+from django.db.utils import IntegrityError
+
+from .models import MainPersonData, BodyMassIndex, User
+
+TEST_USER_NAME = 'test_user'
+TEST_EMAIL = 'test@mail.ru'
+TEST_PASSWORD = 'test_password'
+TEST_AGE = 22
+TEST_HEIGHT = 180
+TEST_WEIGHT = 180
+TEST_SEX = MainPersonData.MALE
+TEST_RESULT = Decimal('55.56')
+TEST_WRONG_AGE = 13
+
+
+def save_test_user(name, email, password) -> User:
+    user = User.objects.create_user(username=name, email=email, password=password)
+    return user
+
+
+def save_test_data(name, sex, age, height, weight) -> User:
+    user = save_test_user(TEST_USER_NAME, TEST_EMAIL, TEST_PASSWORD)
+    MainPersonData.objects.create(
+        person=user,
+        name=name,
+        sex=sex,
+        age=age,
+        height=height,
+        weight=weight
+    )
+    return user
+
+
+class BodyMassIndexTest(TestCase):
+    def setUp(self) -> None:
+        save_test_data(TEST_USER_NAME, TEST_SEX,
+                       TEST_AGE, TEST_HEIGHT, TEST_WEIGHT)
+
+    def test_calculate_body_mass_index(self) -> None:
+        user = User.objects.get_by_natural_key(TEST_USER_NAME)
+        body_mass_index = BodyMassIndex.objects.get(person=user)
+        self.assertEqual(body_mass_index.value, TEST_RESULT)
+
+
+class MainPersonDataTest(TransactionTestCase):
+    def test_constraint(self) -> None:
+
+        with self.assertRaises(IntegrityError):
+            save_test_data(TEST_USER_NAME, TEST_SEX,
+                           TEST_WRONG_AGE, TEST_HEIGHT,
+                           TEST_WEIGHT)
