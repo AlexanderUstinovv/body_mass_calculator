2c9793ede7238d80864b05f0ffbb4e3b93b55b4a
Alexander Ustinov <alexander.a.ustinov@gmail.com>
2020-03-04 11:28:12 +0300

    Added main data from

---
+++ body_mass_calculator/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+
+from .models import MainPersonData
+
+
+class MainDataForm(ModelForm):
+    class Meta:
+        model = MainPersonData
+        fields = ['name', 'age', 'sex', 'age', 'height', 'weight']
